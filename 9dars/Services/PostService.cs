a3f59f8284f9db8596a1108b623bef012f16d615
Nasiba <nasibaabdulboriyeva71@gmail.com>
2024-11-23 12:50:56 +0500

    post adedd

--- 9dars/Services/PostService.cs
+++ 9dars/Services/PostService.cs
@@ -1,12 +1,113 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using _9_dars.Api.Models;
+using System.Reflection;
 
-namespace _9dars.Services
+namespace _9_dars.Api.Services;
+
+public class PostService
 {
-    internal class PostService
+    public List<Post> posts;
+
+    public PostService()
     {
+        posts = new List<Post>();
+    }
+
+    public Post AddPost(Post post)
+    {
+        post.Id = Guid.NewGuid();
+        posts.Add(post);
+
+        return post;
+    }
+
+    public Post GetPostById(Guid postId)
+    {
+        foreach (var post in posts)
+        {
+            if(post.Id == postId)
+            {
+                return post;
+            }
+        }
+
+        return null;
+    }
+
+    public bool DeletePost(Guid postId)
+    {
+        var PostsFromDb = GetPostById(postId);
+        if (PostsFromDb is null)
+        {
+            return false;
+        }
+        posts.Remove(PostsFromDb);
+        return true;
+    }
+
+
+    public bool UpdatePost(Post post)
+    {
+        var PostsFromDb = GetPostById(post.Id);
+        if (PostsFromDb is null)
+        {
+            return false;
+
+        }
+        var index = posts.IndexOf(PostsFromDb);
+        posts[index] = post;
+        return true;
+    }
+
+    public List<Post> GetAllPosts()
+    {
+        return posts;
+    }
+
+
+
+
+
+
+
+    public Post GetMostViewedPost()
+    {
+        var mostViewedPost = posts[0];
+
+        for (var i = 1; i < posts.Count; i++)
+        {
+            if (mostViewedPost.QuantityViews < posts[i].QuantityViews)
+            {
+                mostViewedPost = posts[i];
+            }
+        }
+        return mostViewedPost;
+    }
+
+    public Post GetMostLikedPost()
+    {
+        var mostLikedPost = posts[0];
+        for (var i = 1; i < posts.Count; i++)
+        {
+            if (mostLikedPost.QuantityLikes < posts[i].QuantityLikes)
+            {
+                mostLikedPost = posts[i];
+            }
+        }
+        return mostLikedPost;
+    }
+
+    public Post GetMostCommentedPost()
+    {
+
+    }
+
+    public List<Post> GetPostsByComment(string comment)
+    {
+        
+
+        return ;
     }
 }
+}
+
+
